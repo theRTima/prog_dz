0f7731a546e7b0d652206e0df183d38f33382cd3
timur <roschintimur@gmail.com>
2025-10-06 22:55:26 +0300

    modified:   mid/multip_table.py

---
+++ mid/multip_table.py
@@ -0,0 +1,12 @@
+first_num = input()
+second_num = input()
+
+answer = int(first_num) * int(second_num)
+bin_answer = bin(answer)
+oct_answer = oct(answer)
+hex_answer = hex(answer)
+
+print(answer)
+print(bin_answer)
+print(oct_answer)
+print(hex_answer)
