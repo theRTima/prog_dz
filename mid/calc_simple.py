ae829891c505f46338b8762400706294c883c376
timur <roschintimur@gmail.com>
2025-10-06 22:48:16 +0300

    modified:   mid/calc_simple.py

--- mid/calc_simple.py
+++ mid/calc_simple.py
@@ -1,14 +1,14 @@
 def addition(first_num, secong_num):
-    return (first_num + secong_num)
+    return float(first_num + secong_num)
 
 def multiplication(first_num, secong_num):
-    return (first_num * secong_num)
+    return float(first_num * secong_num)
 
 def subtraction(first_num, secong_num):
-    return (first_num - secong_num)
+    return float(first_num - secong_num)
 
 def division(first_num, secong_num):
-    return (first_num / secong_num)
+    return float(first_num / secong_num)
 
 print("введите два числа, выберите действие")
 first_num = float(input())
@@ -21,26 +21,28 @@ while True:
         first_check = False
     else:
         print("введите число")
-        first_num = float(input())
-    print("выберите действие")
-    print("1 - + ")
-    print("2 - * ")
-    print("3 - - ")
-    print("4 - / ")
+        second_num = float(input())
 
-    choice = int(input())
+    print("действия")
+    print("+")
+    print("*")
+    print("-")
+    print("/")
 
-    if choice == 1:
+    choice = input()
+
+    if choice == "+":
         print(addition(first_num, second_num))
-        second_num = addition(first_num, second_num)
-    elif choice == 2:
+        first_num = addition(first_num, second_num)
+    elif choice == "*":
         print(multiplication(first_num, second_num))
-        second_num = multiplication(first_num, second_num)
-    elif choice == 3:
+        first_num = multiplication(first_num, second_num)
+    elif choice == "-":
         print(subtraction(first_num, second_num))       
-        second_num = subtraction(first_num, second_num) 
-    elif choice == 4:
+        first_num = subtraction(first_num, second_num) 
+    elif choice == "/" and second_num != 0:
         print(division(first_num, second_num))
-        second_num = division(first_num, second_num)    
+        first_num = division(first_num, second_num)    
     else:
-        print("ошибка ввода") 
+        print("ошибка ввода")
+        exit()
