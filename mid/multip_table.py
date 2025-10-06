68ce28a22f3e51e71801be693e667c083eba6816
timur <roschintimur@gmail.com>
2025-10-06 23:11:15 +0300

    modified:   mid/multip_table.py

--- mid/multip_table.py
+++ mid/multip_table.py
@@ -1,13 +1,27 @@
-first_num = int(input())
-second_num = int(input())
-base = int(input())
+first_num = int(input("первое число "))
+second_num = int(input("второе число "))
+base = int(input("основание системы "))
 
 answer = int(first_num) * int(second_num)
-bin_answer = bin(answer)[2:]
-oct_answer = oct(answer)[2:]
-hex_answer = hex(answer)[2:]
 
-print(answer)
-print(bin_answer)
-print(oct_answer)
-print(hex_answer)
+if base == 2:
+    first_num = bin(first_num)[2:]
+    second_num = bin(second_num)[2:] 
+    answer = bin(answer)[2:]
+elif base == 8:
+    first_num = oct(first_num)[2:]
+    second_num = oct(second_num)[2:]
+    answer = oct(answer)[2:]
+elif base == 16:
+    first_num = hex(first_num)[2:]
+    second_num = hex(second_num)[2:]
+    answer = hex(answer)[2:]
+elif base == 10:
+    print("число 1 в системе с основанием", base, ":", first_num)
+    print("число 2 в системе с основанием", base, ":", second_num)
+    print("произведение в системе с основанием", base, ":", answer)
+    exit()
+
+print("число 1 в системе с основанием", base, ":", first_num)
+print("число 2 в системе с основанием", base, ":", second_num)
+print("произведение в системе с основанием", base, ":", answer)
