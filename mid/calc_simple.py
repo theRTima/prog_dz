131104591d1ce5d68e32f5f7db111a7ad1a77322
timur <roschintimur@gmail.com>
2025-10-06 22:20:48 +0300

    renamed:    mid/calc.py -> mid/calc_better.py 	new file:   mid/calc_simple.py

---
+++ mid/calc_simple.py
@@ -0,0 +1,13 @@
+def addition(first_num, secong_num):
+    return (first_num + secong_num)
+
+def multiplication(first_num, secong_num):
+    return (first_num * secong_num)
+
+def subtraction(first_num, secong_num):
+    return (first_num - secong_num)
+
+def division(first_num, secong_num):
+    return (first_num / secong_num)
+
+print("введите два числа, после выберите действие")
