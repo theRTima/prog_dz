81f8b2e741bd341c3509be401b0732fc168ad252
timur <roschintimur@gmail.com>
2025-10-07 10:46:39 +0300

    modified:   hard/euclid_alg.py

--- hard/euclid_alg.py
+++ hard/euclid_alg.py
@@ -1,10 +1,12 @@
 first_num = int(input())
 second_num = int(input())
 
-while first_num != 0 and second_num != 0:
-    if first_num > second_num:
-        first_num = first_num % second_num
-    else:
-        second_num = second_num % first_num
+def gcd(first_num, second_num):
+    while first_num != 0 and second_num != 0:
+        if first_num > second_num:
+            first_num = first_num % second_num
+        else:
+            second_num = second_num % first_num
+    return first_num + second_num
 
-print(first_num+second_num)
+gcd(first_num,second_num)
