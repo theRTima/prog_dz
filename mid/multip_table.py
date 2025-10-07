6989a6b58fda3835bb565ef884b37de7efb2089d
timur <roschintimur@gmail.com>
2025-10-07 09:34:13 +0300

    modified:   mid/multip_table.py

--- mid/multip_table.py
+++ mid/multip_table.py
@@ -2,6 +2,12 @@ first_num = int(input("первое число "))
 second_num = int(input("второе число "))
 base = int(input("основание системы "))
 
+def converter(number, base):
+    all_base = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    if base > 36:
+        print("основание системы больше 36")
+
 answer = int(first_num) * int(second_num)
 
 if base == 2:
