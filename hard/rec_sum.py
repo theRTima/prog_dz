fca83df7f167c3c4f161369350202a307d3a3567
timur <roschintimur@gmail.com>
2025-10-07 10:12:30 +0300

    modified:   hard/rec_sum.py

--- hard/rec_sum.py
+++ hard/rec_sum.py
@@ -1,1 +1,11 @@
 num = int(input())
+
+def recursive_sum(num):
+    numbers_array = list(str(num))
+    sum = 0
+
+    for i in numbers_array:
+        sum += numbers_array[i]
+    return recursive_sum(sum)
+
+recursive_sum(num)
