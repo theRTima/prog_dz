444b314127cb9db6a098b070f2b84321108bca8d
timur <roschintimur@gmail.com>
2025-10-07 10:42:58 +0300

    modified:   hard/euclid_alg.py

--- hard/euclid_alg.py
+++ hard/euclid_alg.py
@@ -1,2 +1,10 @@
 first_num = int(input())
 second_num = int(input())
+
+while first_num != 0 and second_num != 0:
+    if first_num > second_num:
+        first_num = first_num % second_num
+    else:
+        second_num = second_num % first_num
+
+print(first_num+second_num)
