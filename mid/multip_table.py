330d2e4ab3fe3809199fb4040ab4da1345f4e77d
timur <roschintimur@gmail.com>
2025-10-07 09:44:21 +0300

    modified:   mid/multip_table.py

--- mid/multip_table.py
+++ mid/multip_table.py
@@ -25,24 +25,6 @@ def converter(num, base):
     else:
         return result
 
-if base == 2:
-    first_num = bin(first_num)[2:]
-    second_num = bin(second_num)[2:] 
-    answer = bin(answer)[2:]
-elif base == 8:
-    first_num = oct(first_num)[2:]
-    second_num = oct(second_num)[2:]
-    answer = oct(answer)[2:]
-elif base == 16:
-    first_num = hex(first_num)[2:]
-    second_num = hex(second_num)[2:]
-    answer = hex(answer)[2:]
-elif base == 10:
-    print("число 1 в системе с основанием", base, ":", first_num)
-    print("число 2 в системе с основанием", base, ":", second_num)
-    print("произведение в системе с основанием", base, ":", answer)
-    exit()
-
 first_num = int(converter(first_num,base))
 second_num = int(converter(second_num,base))
 answer = first_num*second_num
