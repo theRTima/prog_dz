5a6acc728a6054077b5052f26f7bb52014a3b505
theRTima <roschintimur@gmail.com>
2025-11-06 02:53:07 +0300

    new file:   lab3/mid/book.py 	new file:   lab3/mid/file.py 	new file:   lab3/mid/stat.py

---
+++ lab3/mid/book.py
@@ -0,0 +1,7 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+    
+    def __str__(self):
+        return f"Book: '{self.title}', by author: {self.author}"
