9a46315ad0ee8b7bb09146b97e5edb5b9e174d98
rtima <roschintimur@gmail.com>
2025-11-20 16:23:22 +0300

    modified:   lab4/mid/map_sqr.py

--- lab4/mid/map_sqr.py
+++ lab4/mid/map_sqr.py
@@ -1,1 +1,8 @@
+def map_sqr():
+    numbers = list(range(1, 11))
+    squares = list(map(lambda x: x**2, numbers))
+    return squares
 
+result = map_sqr()
+for i,square in enumerate(result):
+    print(f"{i+1}^2 = {square}")
