f2f4743d07a505a3ac8c2edfde7855af7470f898
theRTima <roschintimur@gmail.com>
2025-12-13 03:36:46 +0300

    modified:   lab5/hard/pyqt_table.py 	new file:   lab6/mid/fast_api_hello.py 	new file:   lab6/mid/flask_hello.py

--- lab5/hard/pyqt_table.py
+++ lab5/hard/pyqt_table.py
@@ -1,8 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QTableWidget, QTableWidgetItem
 
-
-
 class Table(QWidget):
     def __init__(self):
         super().__init__()
