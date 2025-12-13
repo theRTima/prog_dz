968747f34fdc28ae351a66f97db4848017da33ce
theRTima <roschintimur@gmail.com>
2025-12-13 04:01:12 +0300

    modified:   lab6/mid/fast_api_hello.py

--- lab6/mid/fast_api_hello.py
+++ lab6/mid/fast_api_hello.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get('/')
 async def hello():
-    return {"message": "Hello from FastAPI!"}
+    return {"message": "fastapi test"}
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
