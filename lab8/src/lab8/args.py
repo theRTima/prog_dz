163d1aff72b3c83b296681bc8f34393a3bb79476
theRTima <roschintimur@gmail.com>
2025-12-23 15:59:27 +0300

    modified:   lab8/pyproject.toml 	new file:   lab8/src/lab8/args.py

---
+++ lab8/src/lab8/args.py
@@ -0,0 +1,8 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="greeting from timur")
+parser.add_argument("name", help="Your name")
+
+args = parser.parse_args()
+
+print(f"{args.name} - hello from Timur")
