0f22a065426ac0d997cac72fd1b0f8a40a85b2d0
theRTima <roschintimur@gmail.com>
2025-12-23 16:04:03 +0300

    new file:   lab8/poetry.lock 	modified:   lab8/src/lab8/args.py

--- lab8/src/lab8/args.py
+++ lab8/src/lab8/args.py
@@ -1,8 +1,12 @@
-import argparse
+def main():
+    import argparse
 
-parser = argparse.ArgumentParser(description="greeting from timur")
-parser.add_argument("name", help="Your name")
+    parser = argparse.ArgumentParser(description="greeting from timur")
+    parser.add_argument("name", help="Your name")
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-print(f"{args.name} - hello from Timur")
+    print(f"{args.name} - hello from Timur")
+
+if __name__ == "__main__":
+    main()
